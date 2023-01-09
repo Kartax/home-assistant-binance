0b2d0d783366d6e9ea7d1f19e078fe7873598dff
Kartax <philipp@cuntz.org>
2023-01-09 08:18:02 +0000

    restructuring and handle request exception

---
+++ custom_components/binance/binance_ticker_sensor.py
@@ -0,0 +1,62 @@
+"""binance_ticker_sensor"""
+
+import logging
+from datetime import timedelta
+import requests
+from requests import RequestException
+from homeassistant.helpers.entity import Entity
+from homeassistant.const import (
+    STATE_UNKNOWN, DEVICE_CLASS_MONETARY
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+class BinanceTickerSensor(Entity):
+
+    def __init__(self, symbol):
+        self._attr_device_class = DEVICE_CLASS_MONETARY
+        self._name = "Binance Ticker "+symbol.upper()
+        self._symbol = symbol
+        self._state = STATE_UNKNOWN
+        self._data = {}
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def symbol(self):
+        return self._symbol
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def extra_state_attributes(self):
+        return self._data
+
+    async def async_added_to_hass(self):
+        self.hass.helpers.event.async_track_time_interval(
+            self.update, timedelta(seconds=60)
+        )
+
+    def update(self):
+        logger.debug("Updating %s", self._name)
+        
+        url = "https://api.binance.com/api/v3/ticker?symbol="+self._symbol
+        
+        try:
+            response = requests.request("GET", url, headers={}, data={}, timeout=5)
+            
+            if response.status_code != 200:
+                raise RequestException(response.json())
+            
+            self._data = response.json()
+            self._state = float(self._data['lastPrice'])
+            
+        except RequestException as request_exception:
+            logger.error("Error updating %s - %s", self._name, request_exception)
+        
--- custom_components/binance/sensor.py
+++ custom_components/binance/sensor.py
@@ -1,20 +1,17 @@
 """sensor"""
 
 import logging
-from datetime import timedelta
 import voluptuous as vol
-import requests
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.helpers.entity import Entity
-from homeassistant.const import (
-    STATE_UNKNOWN, DEVICE_CLASS_MONETARY
-)
 from .const import (
     CONF_SYMBOLS
 )
 
-_LOGGER = logging.getLogger(__name__)
+from .binance_ticker_sensor import BinanceTickerSensor
+
+
+logger = logging.getLogger(__name__)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
@@ -26,43 +23,5 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     symbols = config.get(CONF_SYMBOLS)
 
     for symbol in symbols:
-        _LOGGER.debug("Setup BinanceSensor: %s", symbol)
-        add_entities([BinanceSensor(symbol)], True)
-
-class BinanceSensor(Entity):
-
-    def __init__(self, symbol):
-        self._attr_device_class = DEVICE_CLASS_MONETARY
-        self._name = "Binance "+symbol.upper()
-        self._symbol = symbol
-        self._state = STATE_UNKNOWN
-        self._data = {}
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def symbol(self):
-        return self._symbol
-
-    @property
-    def state(self):
-        return self._state
-
-    @property
-    def extra_state_attributes(self):
-        return self._data
-
-    async def async_added_to_hass(self):
-        self.hass.helpers.event.async_track_time_interval(
-            self.update, timedelta(seconds=60)
-        )
-
-    def update(self):
-        _LOGGER.debug("Updating: %s", self._name)
-        
-        url = "https://api.binance.com/api/v3/ticker?symbol="+self._symbol
-        response = requests.request("GET", url, headers={}, data={}, timeout=5)
-        self._data = response.json()
-        self._state = self._data['lastPrice']
+        logger.debug("Setup BinanceTickerSensor %s", symbol)
+        add_entities([BinanceTickerSensor(symbol)], True)
