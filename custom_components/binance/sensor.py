70431e67cecffdd439a1bfb5c2368171f79b07f9
Philipp Cuntz <philipp@cuntz.org>
2026-05-31 18:41:19 +0000

    refactor: activate wallet sensors implicitly when api_key is set (v1.3.0b5)

--- custom_components/binance/sensor.py
+++ custom_components/binance/sensor.py
@@ -10,9 +10,7 @@ from .const import (
     CONF_UPDATE_INVERVAL,
     CONF_API_KEY,
     CONF_API_SECRET,
-    CONF_WALLET,
     CONF_WALLET_ASSETS,
-    CONF_WALLET_EARN,
     CONF_WALLET_EARN_ASSETS,
 )
 
@@ -32,9 +30,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Optional(CONF_UPDATE_INVERVAL, default=60): cv.positive_int,
         vol.Optional(CONF_API_KEY): cv.string,
         vol.Optional(CONF_API_SECRET): cv.string,
-        vol.Optional(CONF_WALLET, default=False): cv.boolean,
         vol.Optional(CONF_WALLET_ASSETS): vol.All(cv.ensure_list, [cv.string]),
-        vol.Optional(CONF_WALLET_EARN, default=False): cv.boolean,
         vol.Optional(CONF_WALLET_EARN_ASSETS): vol.All(cv.ensure_list, [cv.string]),
     }
 )
@@ -46,29 +42,20 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     update_interval = config.get(CONF_UPDATE_INVERVAL)
     api_key = config.get(CONF_API_KEY)
     api_secret = config.get(CONF_API_SECRET)
-    wallet = config.get(CONF_WALLET, False)
     wallet_assets = config.get(CONF_WALLET_ASSETS, [])
-    wallet_earn = config.get(CONF_WALLET_EARN, False)
     wallet_earn_assets = config.get(CONF_WALLET_EARN_ASSETS, [])
 
     for symbol in symbols:
         logger.debug("Setup BinanceTickerSensor %s %s %s", symbol, decimals, update_interval)
         add_entities([BinanceTickerSensor(symbol, decimals, update_interval)], True)
 
-    if (wallet or wallet_earn) and not (api_key and api_secret):
-        logger.error(
-            "Binance 'wallet' or 'wallet_earn' requires both 'api_key' and 'api_secret'."
-        )
-        return
-
-    if wallet:
+    if api_key and api_secret:
         logger.debug("Setup BinanceWalletTotalSensor")
         add_entities([BinanceWalletTotalSensor(api_key, api_secret, decimals, update_interval)], True)
         for asset in wallet_assets:
             logger.debug("Setup BinanceWalletSensor %s", asset)
             add_entities([BinanceWalletSensor(asset, api_key, api_secret, decimals, update_interval)], True)
 
-    if wallet_earn:
         logger.debug("Setup BinanceEarnTotalSensor")
         add_entities([BinanceEarnTotalSensor(api_key, api_secret, decimals, update_interval)], True)
         for asset in wallet_earn_assets:
